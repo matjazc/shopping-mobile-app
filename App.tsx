000dccd80c135d0fd71ccf7ff02b75df56df7887
Matthew Cvek <matthew.cvek@centrality.ai>
2020-09-11 23:21:12 +1200

    add app.tsx store link

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { Provider as StoreProvider } from 'react-redux';
+import store from './redux/store';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -14,10 +15,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <StoreProvider store={store}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </StoreProvider>
     );
   }
 }
