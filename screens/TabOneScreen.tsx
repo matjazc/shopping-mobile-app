676761fdc9c08a23906428687e0eb1ccf34bf682
Matthew Cvek <matthew.cvek@centrality.ai>
2020-09-12 01:05:06 +1200

    add shop and set types

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,40 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD_TO_CART, SET_TOTAL, IProductItem, RootState } from '../redux/types';
+import { products } from '../constants/products'
+import { View } from '../components/Themed';
+import { Card, Button, Text } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function TabOneScreen() {
+  const totalCartItems = useSelector((state: RootState)  => state.totalCartItems)
+
+  const dispatch = useDispatch()
+  const addItemToCart = (item: IProductItem) => {
+    dispatch({ type: ADD_TO_CART, payload: item })
+    dispatch({ type: SET_TOTAL })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View>
+      <Text style={{color:'#ffffff', fontSize: 20}}># of products in the cart: {totalCartItems}</Text>
+      <ScrollView>
+      {
+        products.map((item: IProductItem, index: number) => {
+          return (
+            <Card key={index} >
+              <Card.Title>{item.name}</Card.Title>
+              <Text>Price: {item.price} $</Text>
+              <Button
+                title="Add to Cart"
+                onPress={() => addItemToCart(item)}
+              />
+            </Card>
+          );
+        })
+      }
+      </ScrollView>
     </View>
   );
 }
